3c313d518970a60f2a799bacdb1b94155b1e1cb3
tsukamon <69575450+tsukamon@users.noreply.github.com>
2020-08-31 16:03:32 +0900

    アソシエーションの記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+   has_many :items
 
   validates :nickname, :family_name, :first_name, :family_name_katakana, :first_name_katakana, :birthday, presence: true
   validates :password, format: { with: /([0-9].*[a-zA-Z]|[a-zA-Z].*[0-9])/, message: 'Include both letters and numbers' }
   validates :family_name, :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'Full-width characters' }
   validates :family_name_katakana, :first_name_katakana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'Full-width katakana characters' }
+
 end
