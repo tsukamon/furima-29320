0b7982950e604f4326563275b46e785e5928162c
tsukamon <69575450+tsukamon@users.noreply.github.com>
2020-09-01 11:01:54 +0900

    商品登録実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,5 +5,17 @@ class ItemsController < ApplicationController
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      redirect_to  new_item
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name,:explanation,:category_id,:status_id,:delivery_fee_id,:prefecture_id,:arrival_date_id,:price).merge(user_id: current_user.id)
   end
 end
