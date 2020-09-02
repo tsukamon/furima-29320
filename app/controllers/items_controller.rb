4a564e9a090789f49bb7267548a1544b093d0931
tsukamon <69575450+tsukamon@users.noreply.github.com>
2020-09-02 12:12:00 +0900

    indexアクションをitems_controllerに変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -13,6 +13,10 @@ class ItemsController < ApplicationController
     end
   end
 
+  def index
+    @item = Item.all
+  end
+
   private
 
   def item_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "homes#index"
-  resources :items, only: [:new, :create]
+  # root to: "homes#index"
+  root to: "items#index"
+  resources :items, only: [:new, :create, ]
 end
