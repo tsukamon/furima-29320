7117af820c435be8ce3f787edcc6e906e2c54b11
tsukamon <69575450+tsukamon@users.noreply.github.com>
2020-09-03 18:58:43 +0900

    コメントに対する修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -34,8 +34,10 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    @item.destroy
-    redirect_to root_path
+    if @item.destroy
+      redirect_to root_path
+    else
+      render 'show'
   end
 
   private
