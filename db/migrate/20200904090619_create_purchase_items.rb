6d721fb9b078c2b2cd1b244c4029ba52ba5092b9
tsukamon <69575450+tsukamon@users.noreply.github.com>
2020-09-04 19:08:50 +0900

    各テーブルの作成

--- db/migrate/20200904090619_create_purchase_items.rb
+++ db/migrate/20200904090619_create_purchase_items.rb
@@ -1,7 +1,8 @@
 class CreatePurchaseItems < ActiveRecord::Migration[6.0]
   def change
     create_table :purchase_items do |t|
-      
+      t.references :user,         null: false, foreign_key: true
+      t.references :item,         null: false, foreign_key: true
       t.timestamps
     end
   end
