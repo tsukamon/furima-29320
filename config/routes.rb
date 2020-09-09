d0746bc14483fd32cbe150f2bc3279b293879c0a
tsukamon <69575450+tsukamon@users.noreply.github.com>
2020-09-09 17:43:58 +0900

    コメントに対する修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
-  resources :items, only: [:new, :create, :show, :edit, :update, :destroy] do
+  resources :items do
     resources :purchase_items, only: [:index, :new, :create]
   end
 end
