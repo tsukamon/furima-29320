3709b6ee0f9a61716861eb9c257d6a131965cf81
tsukamon <69575450+tsukamon@users.noreply.github.com>
2020-09-11 19:10:37 +0900

    複数の画像が投稿可能

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,7 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one :purchase_item
   has_one :order
-  has_one_attached :image
+  has_many_attached :images
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category
@@ -12,7 +12,7 @@ class Item < ApplicationRecord
   belongs_to_active_hash :arrival_date
 
   validates :name, :explanation, :price, :user, presence: true
-  validates :image, presence: true
+  validates :images, presence: true
   validates :category_id, :status_id, :delivery_fee_id, :prefecture_id, :arrival_date_id, numericality: { other_than: 1, message: 'select' }
   validates :price, format: { with: /\A[0-9]+\z/, message: 'Half-width number' }
   validates :price, numericality: { greater_than: 300, less_than: 9_999_999, message: 'Out of setting range' }
