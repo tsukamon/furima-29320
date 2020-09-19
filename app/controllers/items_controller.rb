0e47c58331608c8ceaa5cb5bda5d22d42a05c1c8
tsukamon <69575450+tsukamon@users.noreply.github.com>
2020-09-19 18:18:12 +0900

    画像投稿改変

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -44,7 +44,7 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:name, :explanation, :category_id, :status_id, :delivery_fee_id, :prefecture_id, :arrival_date_id, :price, images: []).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :explanation, :category_id, :status_id, :delivery_fee_id, :prefecture_id, :arrival_date_id, :price, :image).merge(user_id: current_user.id)
   end
 
   def set_item
